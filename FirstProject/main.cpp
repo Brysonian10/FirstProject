45b5d790c6b14035e991b3f4f01b1d11e652a417
Bryson Spaulding <brysonian10@hotmail.com>
2019-01-22 09:14:25 -0700

    created the c++ project.

---
+++ FirstProject/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FirstProject
+//
+//  Created by Spaulding, Bryson on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "This is confusing\n";
+    return 0;
+}
